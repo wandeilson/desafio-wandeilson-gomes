1e82b6f7ef96c0acb87540c9c51dcbdb52bd55f7
Wandeilson Gomes da Silva <wandeilsongomes@gmail.com>
2023-08-15 22:40:31 -0300

    Refatorando codigo e finalizando projeto.

--- src/caixa-da-lanchonete.js
+++ src/caixa-da-lanchonete.js
@@ -1,83 +1,73 @@
 class CaixaDaLanchonete {
 
     calcularValorDaCompra(metodoDePagamento, itens) {
+        const erroItemSemPedidoPrincipal = "Item extra não pode ser pedido sem o principal";
+       
+        const formasPagamentoDesconto = new Map([
+            ['dinheiro', -0.05],
+            ['credito', 0.03],
+            ['debito', 0.00],
+          ]);
+
+        const cardapio = new Map([
+            ['cafe', 3.00],
+            ['chantily', 1.50],
+            ['suco', 6.20],
+            ['sanduiche', 6.50],
+            ['queijo', 2.00],
+            ['salgado', 7.25],
+            ['combo1', 9.50],
+            ['combo2', 7.50],
+          ]);
+
         if(itens.length == 0 ){
-          return ("Não há itens no carrinho de compra!");
-        }
-        let temItemIvalido = false;
-        const menu = ["cafe", "chantily","suco","sanduiche","queijo","salgado","combo1","combo2"];
-        let valorTotal = 0;
-        for(let i = 0; i <= itens.length-1; i++){
-            let item = itens[i];
-            let itemArray = item.split(',');
-            item = itemArray[0];
-            let qtdItem = itemArray[1];
-            if(qtdItem == 0 ){
-                return "Quantidade inválida!";
-            }
-            if(item == menu[0]){
-                valorTotal += (qtdItem*3.0);
-            }
-            else if(item == menu[1]){
-                if(itens.toString().includes("cafe")){
-                    valorTotal += (qtdItem*1.5);
-                }else{
-                    return "Item extra não pode ser pedido sem o principal";
-                }
-                
-            }
-            else if(item == menu[2]){
-                valorTotal += (qtdItem*6.2);
-            }
-            else if(item == menu[3]){
-                valorTotal += (qtdItem*6.5);
-            }
-            else if(item == menu[4]){
-                if(itens.toString().includes("sanduiche")){
-                    valorTotal += (qtdItem*2.0);
-                }else{
-                    return "Item extra não pode ser pedido sem o principal";
-                }
-                
-            }
-            else if(item == menu[5]){
-                valorTotal += (qtdItem*7.25);
-            }
-            else if(item == menu[6]){
-                valorTotal += (qtdItem*9.50);
-            }
-            else if(item == menu[7]){
-                valorTotal += (qtdItem*7.50);
-            }else{
-                temItemIvalido = true;
-            }
+          return "Não há itens no carrinho de compra!";
         }
 
-        if(temItemIvalido){
-            return "Item inválido!";
-        } 
-           
-        if(metodoDePagamento == "dinheiro"){
-            valorTotal -= (valorTotal*0.05);
-        }else if(metodoDePagamento == "credito"){
-            valorTotal += (valorTotal*0.03);
-        }else if(metodoDePagamento == "debito"){
+        let subTotal = 0;
+        for(let i = 0; i <= itens.length-1; i++){
+            let item = itens[i];
+
+            let pedido = item.split(',');
+
+            let codigoItem = pedido[0];
+            let quantidadeItem = pedido[1];
+
+            if(quantidadeItem == 0 ){
+                return "Quantidade inválida!";
+            }
             
-        }else{
+            if(!cardapio.has(codigoItem)) {
+                return "Item inválido!"; 
+            }
+
+            if(codigoItem == "chantily" && !itens.toString().includes("cafe")){
+                return erroItemSemPedidoPrincipal;
+            }
+
+            if(codigoItem == "queijo" && !itens.toString().includes("sanduiche")){
+                return erroItemSemPedidoPrincipal;
+            }
+            subTotal += (quantidadeItem * cardapio.get(codigoItem));
+        }
+           
+        let valorTotal = subTotal;
+
+        const fatorPagamento = formasPagamentoDesconto.get(metodoDePagamento);
+
+        if(fatorPagamento == undefined) {
             return "Forma de pagamento inválida!";
         }
+        valorTotal = subTotal + (subTotal * fatorPagamento)
+
         let valorTotalFormatado = valorTotal.toFixed(2);
         valorTotalFormatado = valorTotalFormatado.replace('.',',');
         return "R$ " + valorTotalFormatado;
     }
-
-
-}
+} 
 
 let caixa = new CaixaDaLanchonete();
 
 console.log(caixa.calcularValorDaCompra("debito",["cafe,1","chantily,2","suco,1","queijo,1","sanduiche,4","salgado,3","combo1,1","combo2,1"]));
-
-//console.log(caixa.calcularValorDaCompra("debito"));
 
 export { CaixaDaLanchonete };
